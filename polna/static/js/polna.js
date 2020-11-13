75b41d7bab37048d1b6b3189e63983c10db86759
kuzxnia <kacper.kuzniarski@gmail.com>
2020-11-13 13:27:37 +0100

    alpha

---
+++ polna/static/js/polna.js
@@ -0,0 +1,8 @@
+console.log('polna')
+
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
---
+++ todo.md
@@ -0,0 +1,2 @@
+1. pole navbar customizowane, wybierane pola wybierane do nich linki
+2. sekcje w db, obrazek z tekstem, ustawiana kolejność
